339d4842865c3e715d1b536a6c93b74822ba8b59
shana-edwards <22479434@student.uwa.edu.au>
2021-08-10 18:57:38 +0800

    adding documentation to dataparser.py

--- app/formatter/dataparser.py
+++ app/formatter/dataparser.py
@@ -2,11 +2,28 @@ from .dataformatter import AbstractDataFormater
 
 
 class DataParser(AbstractDataFormater):
+    '''
+    Class for parsing the data from a text file into individual post/thread strings.
+    '''
 
     def __init__(self, file):
+        '''
+        Constructor for the DataParser class.
+
+        :param self: Instance of the DataParser object
+        :param file: A text file to be parsed
+        '''
         self.file = file
 
     def parse_text(self) -> list[str]:
+        '''
+        Arranges the content of a file into a list of separate question (or answer) 
+        strings based on their date line.
+
+        :param self: Instance of the DataParser object
+        :return threads: The list of strings, where each string is an individual
+            'post'/'thread', ie. question or answer
+        '''
         with open(self.file, 'r') as f:
             line = f.readline()
             line = f.readline()  # Not sure best way to do this, needed to skip the first line
