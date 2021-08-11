4a05dee569727641379af1e8fa5b159d2ad0b166
fraser <frasergilbert184@gmail.com>
2021-08-11 14:34:30 +0800

    Update __init__.py
    removed unnecessary and deleted modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from app.interface import BasicCLI
 from app.domain import UniversalEncoder, questionmatcher
-from app.embedder import SentEmbeddings
-from app.formatter import DataParser
 from app.parser import parseQuestionsAnswersFromFile
+
 
 class App():
     def __init__(self) -> None:
-        questions, answers = parseQuestionsAnswersFromFile('app/testfiles/help2002-2017.txt')
+        questions, answers = parseQuestionsAnswersFromFile(
+            'app/testfiles/help2002-2017.txt')
         questionMatcher = UniversalEncoder(questions, answers)
         self.__cli = BasicCLI(questionMatcher)
 
