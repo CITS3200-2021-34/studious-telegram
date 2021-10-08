310f29c3b8b31c486e211cbb4bad51b52c61bef8
fraser <frasergilbert184@gmail.com>
2021-10-08 13:12:48 +0800

    Update basiccli.py
    Cleaned up some messy code

--- qna/src/app/interface/basiccli.py
+++ qna/src/app/interface/basiccli.py
@@ -102,76 +102,94 @@ class BasicCLI(AbstractUserInterface):
             If the user does not ask a question i.e. presses enter with no questions,
             the program is exited. 
             '''
-            if not question:
-                print("\nThank you for using our program :)\n")
-                break
+            if question == "":
+                pass
 
-            print("\nLoading Suggestions....\n")
-            suggestions, title_vec = self.__matcher.getSuggestions(
-                question, False)
+            else:
+                print("\nLoading Suggestions....\n")
+                suggestions, title_vec = self.__matcher.getSuggestions(
+                    question, "")
 
-            print(f'QUESTIONS: {question}\n')
-
-            for i in range(len(suggestions)):
-                if i >= 10:
-                    break
-                author = "Student"
-                suggested_question = suggestions[i]
-                if(self.__questions[suggested_question]['From'] == "chris.mcdonald@uwa.edu.au"):
-                    author = "Lecturer"
-                if(self.__questions[suggested_question]['From'] == "poster013@student.uwa.edu.au"):
-                    author = "Tutor"
-                print(f"{i + 1}: {suggestions[i]} ({author})")
-            print("")
-            flag = False
-            if questionary.confirm("Would you like to view these suggestions?").ask():
-                num = questionary.checkbox(
-                    'Select questions',
-                    choices=suggestions[:10]
-                ).ask()
-                if len(num) != 0:
-                    flag = True
-                    self.print_question(num[0])
-                    self.print_answers(num[0])
-            if not flag:
-                body_text = questionary.text(
-                    "What is your question?").ask()
-
-                if not body_text:
-                    print("\nThank you for using our nprogram :)\n")
-
-                else:
-                    summarisation = self.__summariser.getSummarisations(
-                        str(body_text))
-                    summarisation = question + summarisation
-                    suggestions, text_vec = self.__matcher.getSuggestions(
-                        summarisation)
-
-                    print(f'QUESTIONS: {question}\n')
-
-                    for i in range(len(suggestions)):
-                        if i >= 10:
-                            break
+                print(f'QUESTIONS: {question}\n')
+                top_questions = []
+                for i in range(len(suggestions)):
+                    if i >= 10:
+                        break
+                    if(suggestions[i][1] < 0.6):
+                        pass
+                    else:
                         author = "Student"
-                        suggested_question = suggestions[i]
+                        suggested_question = suggestions[i][0]
+                        top_questions.append(suggested_question)
                         if(self.__questions[suggested_question]['From'] == "chris.mcdonald@uwa.edu.au"):
                             author = "Lecturer"
                         if(self.__questions[suggested_question]['From'] == "poster013@student.uwa.edu.au"):
                             author = "Tutor"
                         print(f"{i + 1}: {suggestions[i]} ({author})")
+                print("")
+
+                confirm = False
+                if questionary.confirm("Would you like to view these suggestions? (Select 1 suggestion only)").ask():
+                    num = questionary.checkbox(
+                        'Select questions',
+                        choices=top_questions
+                    ).ask()
+                    if len(num) != 0:
+                        flag = True
+                        question = num[0]
+                        self.print_question(question)
+                        self.print_answers(question)
+
+                # If the user declines the suggestions, prompt user to write a body to the question.
+                if not confirm:
+                    body_text = ""
+                    while(body_text == "" and body_text != 'q'):
+                        body_text = questionary.text(
+                            "What is your question?").ask()
+
+                    if body_text == 'q':  # allow user to quit the prompt
+                        print("\nThank you for using our program :)\n")
+                        break
+
+                    # Summarise the response, and find the embedding
+                    summarisation = self.__summariser.getSummarisations(
+                        body_text)
+                    suggestions, text_vec = self.__matcher.getSuggestions(question,
+                                                                          summarisation)
+
+                    # show the top 10 suggestions above threshold
+                    top_questions = []
+                    print(f'QUESTIONS: {question}\n')
+                    for i in range(len(suggestions)):
+                        if i >= 10:
+                            break
+                        if(suggestions[i][1] < 0.1):
+                            pass
+                        else:
+                            author = "Student"
+                            suggested_question = suggestions[i][0]
+                            top_questions.append(suggested_question)
+                            if(self.__questions[suggested_question]['From'] == "chris.mcdonald@uwa.edu.au"):
+                                author = "Lecturer"
+                            if(self.__questions[suggested_question]['From'] == "poster013@student.uwa.edu.au"):
+                                author = "Tutor"
+                            print(f"{i + 1}: {suggestions[i]} ({author})")
                     print("")
 
-                    flag = False
-                    if questionary.confirm("Would you like to view these suggestions?").ask():
+                    # Ask the user whether they wish to view the suggestions
+                    confirm = False
+                    if questionary.confirm("Would you like to view these suggestions? (Select 1 suggestion only)").ask():
                         num = questionary.checkbox(
                             'Select questions',
-                            choices=suggestions[:10]
+                            choices=top_questions
                         ).ask()
                         if len(num) != 0:
-                            flag = True
-                            self.print_question(num[0])
-                            self.print_answers(num[0])
-                    if not flag:
+                            confirm = True
+                            question = num[0]
+                            self.print_question(question)
+                            self.print_answers(question)
+                    if not confirm:
+                        # Write the response to the json file
                         title_vec = title_vec.numpy().tolist()
                         text_vec = text_vec.numpy().tolist()
                         write_to_json(question, body_text,
