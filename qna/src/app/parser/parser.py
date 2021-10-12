9f9f00b161762760b90bb6914636235347ce6003
Amos <61882594+amos123666@users.noreply.github.com>
2021-10-12 22:34:58 +0800

    Update parser.py (#38)

--- qna/src/app/parser/parser.py
+++ qna/src/app/parser/parser.py
@@ -49,6 +49,7 @@ def getPostsFromThreads(threads) -> List[Question]:
     is stored in a json formatted dictionary to be stored in a JSON file
 
     :param threads: List of question/answer strings
+    :param target_model: The target model
     :return None:
     '''
 
